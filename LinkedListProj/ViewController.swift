86775d444c41bae5fa1cc9d3af24656b49a1912f
Sam Titovskiy <samt@openpay.com.au>
2020-04-01 18:48:38 +0300

    proj start

---
+++ LinkedListProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinkedListProj
+//
+//  Created by SEMEN TITOVSKYI on 01.04.2020.
+//  Copyright © 2020 TITO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
